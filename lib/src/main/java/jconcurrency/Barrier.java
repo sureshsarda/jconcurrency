6845a273dc805d2dd9f1b0046ea5fc3a272952af
Suresh Sarda <sureshssarda@gmail.com>
2023-05-07 03:01:44 +0000

    Rendezvous and Barrier

---
+++ lib/src/main/java/jconcurrency/Barrier.java
@@ -0,0 +1,65 @@
+package jconcurrency;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class Barrier {
+    
+    public static void main(String[] args) {
+    
+        /*
+         * At any point a certain number of threads should be able to perform actions in critical section
+         */
+
+        Semaphore s = new Semaphore(5);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            Runnable runnable = new WorkThreadBarrier(s);
+            
+            executorService.submit(runnable);
+        }
+
+        executorService.shutdown();
+    }
+}
+
+
+class WorkThreadBarrier implements Runnable {
+
+    private Semaphore semaphore;
+
+    public WorkThreadBarrier(Semaphore semaphore) {
+        this.semaphore = semaphore;
+
+    }
+
+    @Override
+    public void run() {
+        // System.out.println("Begin: Non Critical Section" + Thread.currentThread().getName());
+        
+        try {
+            semaphore.acquire();
+            // critical section
+
+            System.out.println("Begin: Critical Section" + Thread.currentThread().getName());
+            Thread.sleep(new Random().nextInt(2, 5) * 1000);
+            System.out.println("End: Critical Section" + Thread.currentThread().getName());
+
+            semaphore.release();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // System.out.println("End: Non Critical Section" + Thread.currentThread().getName());
+
+
+    }
+
+
+
+
+}
